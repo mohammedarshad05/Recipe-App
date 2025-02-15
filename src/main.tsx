e6426650fc8075681fcf2ae81a2628ff2c0f0de5
Mohammed Arshad <arshff005@gmail.com>
2025-02-15 10:32:59 +0530

    js added

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 
 createRoot(document.getElementById('root')!).render(
